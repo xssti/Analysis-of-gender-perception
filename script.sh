dd62ac5014d148205e42282cbc803f0006b72f0b
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-12-10 23:14:01 -0500

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-while true; do
-    python3 scraping_articles.py
-    if [ $? -eq 0 ]; then
-        break
-    fi
-    echo 'Code failed, Retrying....'
-    sleep 5
-done
-echo "Sucessfully executed!!"
